6e1a43837857a326b2d317171627a5963f8ab411
Fernado Lima <fernandovlima.s@gmail.com>
2020-02-29 18:38:26 -0300

    toast for errors

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -20,6 +21,7 @@ export function* signIn({ payload }) {
 
     history.push('/deliveries');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados.');
     yield put(signInFailure());
   }
 }
